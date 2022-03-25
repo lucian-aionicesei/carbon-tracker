7ac7b5b8324b2ae9f0750354e280d41d121edcef
siangi <siangi@gmx.ch>
2022-03-25 11:26:42 +0100

    created link to pagespeed API, website carbon doesn't work yet, because of a bug on their side apparently?

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-import './sass/style.scss';
+import "./sass/style.scss";
+
+const URL = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://developers.google.com&key=AIzaSyDVaplgnZ31AzZVkSNiFImxl5WGciW31vg";
+const websiteCarbonApiUrl = "https://api.websitecarbon.com/site?url=https%3A%2F%2Fwww.wholegraindigital.com%2F";
+console.log("test before fetch");
+
+fetch(URL)
+    .then((data) => {
+        return data.json();
+    })
+    .then((data) => console.log(data));
+
+fetch(websiteCarbonApiUrl, { mode: "cors" })
+    .then((data) => {
+        console.log(data);
+        return data.json();
+    })
+    .then((data) => console.log(data));
