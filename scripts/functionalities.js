b5711d8825e2b85dcdf6ea1b31e4fa825e11b3a5
anders iversen <ande397c@stud.kea.dk>
2022-03-29 12:19:33 +0200

    Burger-mneu implemented

--- scripts/functionalities.js
+++ scripts/functionalities.js
@@ -1,9 +1,20 @@
 "use strict";
 window.addEventListener("load", setup);
 
+const burger = document.querySelector(".burger");
+const links = document.querySelector(".nav_links");
+
 function setup() {
   document.querySelector(".dropbtn_industry").addEventListener("click", showContent);
 
+  // Burger menu
+  burger.addEventListener("click", toggle);
+  burger.addEventListener("mouseover", over);
+
+  burger.addEventListener("mouseout", done);
+  const logo = document.querySelector(".logo");
+
+  // Close dropdown content if user clicks outside of it
   document.addEventListener("mouseup", function (e) {
     const container_content = document.querySelector(".dropdown-content");
     if (!container_content.contains(e.target)) {
@@ -12,6 +23,7 @@ function setup() {
     }
   });
 
+  // Eventlistener on dropdown options
   let allIndustryOptions = document.querySelectorAll(".filter_industry");
   allIndustryOptions.forEach((industryOption) => {
     industryOption.addEventListener("click", getIndustry);
@@ -19,6 +31,7 @@ function setup() {
 }
 
 function getIndustry(industryOption) {
+  // Get option value
   const industryChoice = industryOption.target.getAttribute("data-filter");
   console.log(industryChoice);
   document.querySelector(".dropbtn_industry").textContent = industryChoice;
@@ -27,9 +40,25 @@ function getIndustry(industryOption) {
 }
 
 function showContent() {
-  //   document.querySelector(".dropbtn_industry").addEventListener("click", () => {
-  //     document.querySelector(".dropdown-content").classList.remove("flex");
-  //   });
   document.querySelector(".dropdown-content").classList.toggle("flex");
   document.querySelector(".dropbtn_industry").classList.toggle("changeArrow");
+}
+
+function toggle() {
+  if (burger.classList.contains("toggle")) {
+    burger.addEventListener("mouseover", over);
+    document.body.style.overflowY = "visible";
+  } else {
+    burger.removeEventListener("mouseover", over);
+    document.body.style.overflow = "hidden";
+  }
+  links.classList.toggle("nav_toggle");
+  burger.classList.toggle("toggle");
+}
+
+function over() {
+  burger.classList.add("hover");
+}
+function done() {
+  burger.classList.remove("hover");
 }
