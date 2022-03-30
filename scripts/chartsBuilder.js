2c6b29dce9e029e5de9b48a10d0d2d6f85da8b23
siangi <siangi@gmx.ch>
2022-03-30 18:20:12 +0200

    created article for unminified JS, whit dummy data for now

--- scripts/chartsBuilder.js
+++ scripts/chartsBuilder.js
@@ -2,9 +2,10 @@ import Chart from "chart.js/auto";
 
 const colors = ["#9BFFC2", "#FD7484"];
 
-Chart.defaults.borderWidth = "0";
+Chart.defaults.borderWidth = 0;
 Chart.defaults.borderColor = "rgba(0, 0, 0, 0.0)";
 
+// only used for testing, remove when real data comes into play
 export function createTestDonut() {
     const testCanvas = document.querySelector("#overall-donut-chart");
     const testData = [80, 20];
@@ -12,16 +13,15 @@ export function createTestDonut() {
     let chart = createDonutChart(testData, testCanvas);
 }
 
+// only used for testing, remove when real data comes into play
 export function createTestPie() {
     const testCanvas = document.querySelector("#unused-script-chart");
     const testData = [80, 20];
 
     let chart = createPieChart(testData, testCanvas);
-    setTimeout(() => {
-        updateChart(chart, [80]);
-    }, 2000);
 }
 
+// only used for testing, remove when real data comes into play
 export function createTestBars() {
     const testCanvas = document.querySelector("#media-sizes-chart");
     const testData1 = [100, 40, 50, 60, 80];
@@ -34,6 +34,7 @@ export function createTestBars() {
     }, 2000);
 }
 
+// takes an array with numbers and a canvas, creates donut chart
 export function createDonutChart(data, canvas) {
     let donutChart = new Chart(canvas.getContext("2d"), {
         type: "doughnut",
@@ -60,6 +61,7 @@ export function createDonutChart(data, canvas) {
     return donutChart;
 }
 
+// takes a array of numbers and canvas and creates a pie Chart there
 export function createPieChart(data, canvas) {
     let pieChart = new Chart(canvas, {
         type: "pie",
@@ -86,6 +88,8 @@ export function createPieChart(data, canvas) {
     return pieChart;
 }
 
+// groups the corresponding values from dataSet1 and dataSet2 in a bar chart
+// dataSet1 and dataSet2 are arrays of numbers, labels are the group names, canvas is the element to create in.
 export function createCoupledBars(dataSet1, dataset2, labels, canvas) {
     let barsChart = new Chart(canvas, {
         type: "bar",
--- scripts/reportHandler.js
+++ scripts/reportHandler.js
@@ -1,15 +1,41 @@
-import { createDonutChart } from "./chartsBuilder.js";
+import { createDonutChart, createPieChart, updateChart } from "./chartsBuilder.js";
 
 export function initReport(data) {
-    const testData = [
+    const testDataImprovement = [
         { title: "Reduce image Size", percentageData: [55, 45] },
         { title: "Minify code", percentageData: [75, 25] },
         { title: "Use cached files", percentageData: [27, 73] },
     ];
 
-    createImprovementAreas(testData);
+    createImprovementAreas(testDataImprovement);
+    fillunMinifiedJsArticle(20, 80);
 }
 
+// fills the Elements chart and the values in the text,
+// links the chartChange and oder styleChanges to the checkbox/sliders
+function fillunMinifiedJsArticle(whiteSpaceSize, minifiedSize) {
+    const percentage = Math.ceil((whiteSpaceSize / (minifiedSize + whiteSpaceSize)) * 100);
+    const article = document.querySelector("#unminified-js");
+
+    const chart = createPieChart([minifiedSize, whiteSpaceSize], article.querySelector("canvas.chart"));
+    let sizeValContainer = article.querySelector("[data-field=size-vals]");
+    sizeValContainer.textContent = `${whiteSpaceSize}kb or ${percentage}%`;
+
+    let sliderCheckbox = article.querySelector(".toggle-container input[type=checkbox]");
+    let divider = article.querySelector("hr");
+    sliderCheckbox.addEventListener("change", (event) => {
+        if (event.target.checked) {
+            updateChart(chart, [minifiedSize]);
+        } else {
+            updateChart(chart, [minifiedSize, whiteSpaceSize]);
+        }
+
+        divider.classList.toggle("positive", event.target.checked);
+    });
+}
+
+// creates the "biggest Areas of Improvement" section,
+// area data should contain a "title" and "percentageData" to Display
 function createImprovementAreas(areaData) {
     const parent = document.querySelector(".biggest-improvement .sectors");
     const template = document.querySelector("#mostImportantTemplate");
@@ -17,7 +43,6 @@ function createImprovementAreas(areaData) {
 
     for (let i = 0; i < areaData.length; i++) {
         let newSector = template.content.cloneNode(true);
-        console.log(newSector.querySelector("canvas.chart"));
         newSector.querySelector("[data-field=title]").textContent = areaData[i].title;
         newSector.querySelector(".numbered-donut [data-field=number]").textContent = areaData[i].percentageData[0];
 
