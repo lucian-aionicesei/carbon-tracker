734d8d1fbea6669a1832648dda3e73d9f7d9c59a
siangi <siangi@gmx.ch>
2022-03-31 21:14:49 +0200

    created DataModel to read and unify data, got the unused code data out of the pagespeed api. and commented out the fucking websitecarbon api because its broken, again......

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
 import "./sass/style.scss";
 import { initReport } from "./scripts/reportHandler.js";
-import { createTestPie } from "./scripts/chartsBuilder";
-
-const URL = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://developers.google.com&key=AIzaSyDVaplgnZ31AzZVkSNiFImxl5WGciW31vg";
-const websiteCarbonApiUrl = "https://api.websitecarbon.com/site?url=https%3A%2F%2Fwww.wholegraindigital.com%2F";
-
-fetch(URL)
-    .then((data) => {
-        return data.json();
-    })
-    .then((data) => console.log(data));
-
-// fetch(websiteCarbonApiUrl, { mode: "cors" })
-//     .then((data) => {
-//         console.log(data);
-//         return data.json();
-//     })
-//     .then((data) => console.log(data));
+import { getUselessCodeData, loadData } from "./scripts/dataModel";
 
 initReport();
+
+await loadData("https://www.jungwachthorw.ch/");
+console.log(getUselessCodeData());
---
+++ scripts/dataModel.js
@@ -0,0 +1,53 @@
+const pagespeedUrl = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=URLHOLDER&key=AIzaSyDVaplgnZ31AzZVkSNiFImxl5WGciW31vg";
+const websiteCarbonApiUrl = "https://kea-alt-del.dk/websitecarbon/?url=";
+
+let pagespeedData;
+let WebsiteCarbonData;
+
+export async function loadData(testUrl) {
+    // await fetch(websiteCarbonApiUrl + testUrl)
+    //     .then((data) => {
+    //         return data.json();
+    //     })
+    //     .then((data) => {
+    //         WebsiteCarbonData = data;
+    //     });
+
+    await fetch(pagespeedUrl.replace("URLHOLDER", testUrl))
+        .then((data) => {
+            return data.json();
+        })
+        .then((data) => {
+            pagespeedData = data;
+        });
+}
+
+// returns how much code there is overall, and what part is useless
+//(whitespace, unused, legacy) in Kilobytes
+export function getUselessCodeData() {
+    let fullCodeSize = 0;
+    let uselessCodeSize = 0;
+    let baseNode = pagespeedData.lighthouseResult.audits;
+    console.log(baseNode);
+    fullCodeSize = getTotalResourceTypeSize("script") + getTotalResourceTypeSize("stylesheet");
+
+    uselessCodeSize += baseNode["unused-javascript"].details.overallSavingsBytes;
+    uselessCodeSize += baseNode["unused-css-rules"].details.overallSavingsBytes;
+    uselessCodeSize += baseNode["unminified-javascript"].details.overallSavingsBytes;
+    uselessCodeSize += baseNode["unminified-css"].details.overallSavingsBytes;
+
+    return { fullCodeSize: Math.round(fullCodeSize / 1024), uselessCodeSize: Math.round(uselessCodeSize / 1024) };
+}
+
+function getTotalResourceTypeSize(resourceType) {
+    let totalSize = 0;
+    let resources = pagespeedData.lighthouseResult.audits["resource-summary"].details.items.filter((resource) => {
+        return resource.resourceType.toLowerCase() === resourceType.toLowerCase();
+    });
+
+    resources.forEach((resource) => {
+        totalSize += resource.transferSize;
+    });
+
+    return totalSize;
+}
