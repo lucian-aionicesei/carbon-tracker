6edae3bdf6fd15a8ab7294a856a29a4ea1a02014
siangi <siangi@gmx.ch>
2022-04-08 12:46:23 +0200

    charts destroying done

--- scripts/chartsBuilder.js
+++ scripts/chartsBuilder.js
@@ -2,54 +2,23 @@ import Chart from "chart.js/auto";
 
 const colors = ["#9BFFC2", "#FD7484"];
 
-let barsChart;
-let barsChart2;
-let testDonutChart;
-let pieChart;
-let pieChart2;
+let charts = [];
 
 Chart.defaults.borderWidth = 0;
 Chart.defaults.borderColor = "rgba(0, 0, 0, 0.0)";
-
-// only used for testing, remove when real data comes into play
-export function createTestDonut() {
-    const testCanvas = document.querySelector("#overall-donut-chart");
-    const testData = [80, 20];
-
-    testDonutChart = createDonutChart(testData, testCanvas);
-}
-
-// only used for testing, remove when real data comes into play
-export function createTestPie() {
-    const testCanvas = document.querySelector("#unused-script-chart");
-    const testData = [80, 20];
-
-    pieChart = createPieChart(testData, testCanvas);
-}
-
-// only used for testing, remove when real data comes into play
-export function createTestBars() {
-    const testCanvas = document.querySelector("#media-sizes-chart");
-    const testData1 = [100, 40, 50, 60, 80];
-    const testData2 = [81, 20, 40, 50, 60];
-    const labels = ["file1.jpg", "test.png", "fill.mp4", "small.png", "header.png"];
-
-    barsChart = createCoupledBars(testData1, testData2, labels, testCanvas);
-    setTimeout(() => {
-        updateChartData(barsChart, testData2);
-    }, 2000);
-}
 
 // takes an array with numbers and a canvas, creates donut chart
 export function createDonutChart(data, canvas) {
     let donutChart = new Chart(canvas.getContext("2d"), {
         type: "doughnut",
         data: {
-            datasets: [{
-                data: data,
-                backgroundColor: colors,
-                borderWidth: 0,
-            }, ],
+            datasets: [
+                {
+                    data: data,
+                    backgroundColor: colors,
+                    borderWidth: 0,
+                },
+            ],
         },
         options: {
             cutout: "60%",
@@ -62,20 +31,23 @@ export function createDonutChart(data, canvas) {
         },
     });
 
+    charts.push(donutChart);
     return donutChart;
 }
 
 // takes a array of numbers and canvas and creates a pie Chart there
 export function createPieChart(data, canvas) {
-    pieChart2 = new Chart(canvas, {
+    let pieChart = new Chart(canvas, {
         type: "pie",
         data: {
-            datasets: [{
-                label: "ratio of used/unused Javascript",
-                data: data,
-                backgroundColor: colors,
-                borderWidth: 0,
-            }, ],
+            datasets: [
+                {
+                    label: "ratio of used/unused Javascript",
+                    data: data,
+                    backgroundColor: colors,
+                    borderWidth: 0,
+                },
+            ],
         },
         options: {
             responsive: true,
@@ -87,17 +59,19 @@ export function createPieChart(data, canvas) {
         },
     });
 
-    return pieChart2;
+    charts.push(pieChart);
+    return pieChart;
 }
 
 // groups the corresponding values from dataSet1 and dataSet2 in a bar chart
 // dataSet1 and dataSet2 are arrays of numbers, labels are the group names, canvas is the element to create in.
 export function createCoupledBars(dataSet1, dataset2, labels, canvas, stacked = false) {
-    barsChart2 = new Chart(canvas, {
+    let barsChart = new Chart(canvas, {
         type: "bar",
         data: {
             labels: labels,
-            datasets: [{
+            datasets: [
+                {
                     label: "media files currently",
                     data: dataSet1,
                     backgroundColor: colors[1],
@@ -131,7 +105,8 @@ export function createCoupledBars(dataSet1, dataset2, labels, canvas, stacked = 
         },
     });
 
-    return barsChart2;
+    charts.push(barsChart);
+    return barsChart;
 }
 
 export function createStackedBars(canvas, datasetBad, datasetGood, labels) {
@@ -152,17 +127,11 @@ export function updateChartData(chart, newDataSets) {
     chart.update();
 }
 
-
 export function clearAllCharts() {
-    barsChart.destroy();
-    barsChart2.destroy();
-    testDonutChart.destroy();
-    pieChart.destroy();
-    pieChart2.destroy();
+    charts.forEach((chart) => {
+        chart.destroy();
+        // chart.clear();
+    });
 
-    barsChart.clear();
-    barsChart2.clear();
-    testDonutChart.clear();
-    pieChart.clear();
-    pieChart2.clear();
+    charts = [];
 }
